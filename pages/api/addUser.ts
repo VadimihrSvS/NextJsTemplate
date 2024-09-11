53d7421925ee3257695cfc357808f80b51df52d8
VadimirSvS <vadimirsvs@gmail.com>
2024-09-11 23:55:43 +0300

    updated ts

--- pages/api/addUser.ts
+++ pages/api/addUser.ts
@@ -1,7 +1,7 @@
 import prisma from '../../lib/prisma';
 
 // PUT /api/addUser
-export default async function handle(req, res) {
+export async function handle(req, res) {
     const { user } = req.body;
     const addUser = await prisma.user.create({
         data: { name: user, role: "123" },
